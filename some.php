1c5ffd780c7edd4737d643068e39bc9ade234930
Christabel <udodorchristabel@gmail.com>
2024-02-26 17:10:53 -0800

    initial commit

---
+++ some.php
@@ -0,0 +1,17 @@
+<?php
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $to = "your_email@example.com";
+//     $subject = "Contact Form Submission";
+//     $email = $_POST["email"];
+//     $message = $_POST["message"];
+//     $headers = "From: $email";
+    
+//     if (mail($to, $subject, $message, $headers)) {
+//         echo "Email sent successfully!";
+//     } else {
+//         echo "Email sending failed.";
+//     }
+// }
+
+echo" hello world";
+?>
